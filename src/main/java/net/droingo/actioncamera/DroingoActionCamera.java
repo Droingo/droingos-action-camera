94b595455de099046d2d9e04d3bbb7f8e3d195fa
Droingo <cmbmx57@gmail.com>
2026-06-20 12:02:37 +1000

    Initial Droingo Action Camera NeoForge template

---
+++ src/main/java/net/droingo/actioncamera/DroingoActionCamera.java
@@ -0,0 +1,11 @@
+package net.droingo.actioncamera;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(DroingoActionCamera.MOD_ID)
+public final class DroingoActionCamera {
+    public static final String MOD_ID = "droingo_action_camera";
+
+    public DroingoActionCamera() {
+    }
+}
